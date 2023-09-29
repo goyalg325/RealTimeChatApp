7997b0ebd41c1ee6d55c5b6cc0b5097bcc26e69b
goyalg325 <shaffigoyal774@gmail.com>
2023-09-30 01:50:25 +0530

    third

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,16 +1,40 @@
-const io = require('socket.io')(8000)
-const cors = require('cors'); // Require the cors package
-
-// Use cors middleware to allow requests from all origins
-io.use(cors());
-
-const users = {};
-io.on('connection',(socket) => {
-    socket.on('new-user-joined',(name) => {
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined',name);
-    })
-    socket.on('send',(message) => {
-        socket.broadcast.emit('receive',{message: message,name:users[socket.id]})
+const http = require("http")
+const express = require("express")
+const {Server} = require("socket.io")
+const path = require('path')
+const cors = require('cors');
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+// socket.io
+io.on("connection",(socket) => {
+    console.log("a new user has connected",socket.id)
+    socket.on("user-message",(message) => {
+        socket.broadcast.emit("message",message)
     })
 })
+app.use(cors());
+app.use(express.static(path.resolve("./public")))
+app.get("/",(req,res) => {
+    return res.sendFile(path.resolve("./public/index.html"))
+})
+server.listen(8000,() => {
+    console.log('server started at port 8000')
+})
+
+// const io = require('socket.io')(8000)
+// const cors = require('cors'); // Require the cors package
+
+// // Use cors middleware to allow requests from all origins
+// io.use(cors());
+
+// const users = {};
+// io.on('connection',(socket) => {
+//     socket.on('new-user-joined',(name) => {
+//         users[socket.id] = name;
+//         socket.broadcast.emit('user-joined',name);
+//     })
+//     socket.on('send',(message) => {
+//         socket.broadcast.emit('receive',{message: message,name:users[socket.id]})
+//     })
+// })
--- nodeServer/public/client.js
+++ nodeServer/public/client.js
@@ -26,10 +26,32 @@
 // socket.on('user-joined',name => {
 //     apend(`${name} has joined the chat`,'left');
 // })
+const apend = (msg, pos) => {
+    const msgElement = document.createElement("div");
+    msgElement.innerText = msg; // Remove the quotes around "msg"
+    msgElement.classList.add(pos); // Use the variable 'pos' instead of the string "pos"
+
+    const msgContainer = document.getElementById('msgcontainer'); // Use 'getElementById' instead of 'getElementsByID'
+    msgContainer.appendChild(msgElement); // Correct the variable name to 'msgContainer'
+}
+
+
 const socket = io() 
 const sendBtn = document.getElementById('sendBtn')
 const messageInput = document.getElementById('message')
+socket.on("message",(message) => {
+    // jo bhi message aaya hai uska kya krna hai
+    apend(message,'left');
+})
 sendBtn.addEventListener("click",(e) => {
     const message = messageInput.value ;
+    messageInput.value = '';
+    
     console.log(message);
+    // my own expt code
+    // emit se hmne sbke sys pe message bhej diya 
+    //but apne system pe kaise dikhega wo message vo part shayad yahan
+    apend(message,'right')
+    socket.emit("user-message",message);
+   
 })
